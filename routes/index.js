45a639d42774587bc86b7fba1ffa203fa5a4ba62
NOURDINE BENLKAMLE <benlkamle.tiznit.95@gmail.com>
2020-12-16 16:18:34 +0100

    update and  delete todo items

--- routes/index.js
+++ routes/index.js
@@ -14,5 +14,14 @@ module.exports = (app) => {
 
 
   app.post('/api/todos/:todoId/items', todoItemsController.create);
-//   app.get('/api/todos/:todoId/items', todoItemsController.list);
+  app.put('/api/todos/:todoId/items/:todoItemId', todoItemsController.update);
+  app.delete(
+    '/api/todos/:todoId/items/:todoItemId', todoItemsController.destroy
+  );
+
+  // For any other request method on todo items, we're going to return "Method Not Allowed"
+  app.all('/api/todos/:todoId/items', (req, res) =>
+    res.status(405).send({
+      message: 'Method Not Allowed',
+  }));
 };
