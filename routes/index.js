2854e86d2d78c28eb3ea65ab2d9c04ebd0faa2b9
NOURDINE BENLKAMLE <benlkamle.tiznit.95@gmail.com>
2020-12-16 16:04:54 +0100

    retrieve, update and delete todos

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const todosController = require('../controllers').todos;
+const todoItemsController = require('../controllers').todoItems;
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
@@ -7,4 +8,11 @@ module.exports = (app) => {
 
   app.post('/api/todos', todosController.create);
   app.get('/api/todos', todosController.list);
+  app.get('/api/todos/:todoId', todosController.retrieve);
+  app.post('/api/todos/:todoId', todosController.update);
+  app.delete('/api/todos/:todoId', todosController.destroy);
+
+
+  app.post('/api/todos/:todoId/items', todoItemsController.create);
+//   app.get('/api/todos/:todoId/items', todoItemsController.list);
 };
